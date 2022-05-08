1cdc6d4c93bc28201c8f6e7c1e3832dea2359325
Christiano Anderson <chris@christiano.dev>
2022-05-08 09:28:37 +0200

    Added a dummy consumer

--- src/main/java/dev/christiano/app/App.java
+++ src/main/java/dev/christiano/app/App.java
@@ -2,6 +2,7 @@ package dev.christiano.app;
 
 import java.util.List;
 
+import dev.christiano.consumer.FakeUserConsumer;
 import dev.christiano.model.User;
 import dev.christiano.producer.FakeUserProducer;
 import dev.christiano.producer.UserProducer;
@@ -9,11 +10,46 @@ import dev.christiano.producer.UserProducer;
 public class App {
     public static void main(String[] args) {
 
+        if (args.length < 1) {
+
+            System.out.println("Please enter an option: ");
+            System.out.println("producer or consumer");
+
+            System.exit(1);
+        } else {
+            String firstArg = args[0].toString();
+            System.out.println("You entered " + firstArg);
+
+            if (firstArg.equals("consumer")) {
+                System.out.println("Starting the consumer");
+                FakeUserConsumer.ConsumerUser();
+                System.exit(0);
+            }
+            
+            if (firstArg.equals("producer")) {
+                System.out.println("Starting the producer");
+                pushUsers();
+                System.exit(0);
+            } else {
+                System.out.println("Valid options: producer or consumer");
+                System.exit(1);
+            }
+        }
+
+    }
+
+    public static void pushUsers() {
+
+        System.out.println("Staring to publish users...");
+
         List<User> users = FakeUserProducer.batchFakeUser(10);
 
         for (User u : users) {
             UserProducer.publishUser(u);
+            System.out.println("Ingested: " + u);
         }
 
+        System.out.println("Ingestion done!");
     }
+
 }
---
+++ src/main/java/dev/christiano/consumer/FakeUserConsumer.java
@@ -0,0 +1,40 @@
+package dev.christiano.consumer;
+
+import org.apache.pulsar.client.api.Consumer;
+import org.apache.pulsar.client.api.Message;
+import org.apache.pulsar.client.api.PulsarClient;
+
+public class FakeUserConsumer {
+
+    public static void ConsumerUser() {
+
+        try {
+            PulsarClient client = PulsarClient.builder()
+                .serviceUrl("pulsar://localhost:6650")
+                .build();
+
+            Consumer consumer = client.newConsumer()
+                .topic("users-topic")
+                .subscriptionName("javacli")
+                .subscribe();
+
+            while (true) {
+                Message msg = consumer.receive();
+
+                try {
+                    System.out.println("Message " + new String(msg.getData()));
+                    consumer.acknowledge(msg);
+                } catch (Exception e) {
+                    consumer.negativeAcknowledge(msg);
+                    System.out.println("Error");
+                }
+            }
+
+            
+        } catch (Exception e ) {
+            System.out.println("Error with the client");
+        }
+
+    }
+    
+}
